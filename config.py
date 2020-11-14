eee268b66add730605f840106132f8b993863b51
Askhab Musaev <42musaev@gmail.com>
2020-11-14 10:34:11 +0300

    add config db

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Configuration(object):
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
